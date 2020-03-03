9da14bdc4ccf10442fd986fb1dcf01a8ed5c2be2
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-03-03 15:50:43 -0300

    Added colors constants

---
+++ src/utilities/colors.js
@@ -0,0 +1,12 @@
+const COLORS = {
+  marvelPrimary: '#ED1D24',
+  pageBackground: '#F1F1F1',
+  titleBackground: '#FFDD00',
+  link: '#0099FF',
+  descriptionBackground: '#FFEF9C',
+  placeholderText: 'rgba(0, 0, 0, 0.4)',
+  black: '#000000',
+  white: '#FFFFFF',
+};
+
+export default COLORS;
