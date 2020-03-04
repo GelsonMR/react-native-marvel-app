1622d8c2a9d73d6b53798a496e0b5c59ee24db4e
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-03-04 05:39:17 -0300

    Added input text constants

--- src/utilities/text.js
+++ src/utilities/text.js
@@ -3,6 +3,10 @@ const TEXT = {
     size: 20,
     fontFamily: 'BackIssuesBB-Italic',
   },
+  input: {
+    size: 16,
+    fontFamily: 'BackIssuesBB-Italic',
+  },
   balloon: {
     size: 12,
     fontFamily: 'BackIssuesBB-Italic',
