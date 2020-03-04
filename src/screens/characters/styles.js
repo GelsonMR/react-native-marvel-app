aae4ae449fd50f1efd83e1ff89ac24c1a428aca9
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-03-04 05:40:17 -0300

    Added characters integration related styles

--- src/screens/characters/styles.js
+++ src/screens/characters/styles.js
@@ -1,6 +1,8 @@
 import { StyleSheet } from 'react-native';
+import BORDER from '../../utilities/border';
 import COLORS from '../../utilities/colors';
 import SPACING from '../../utilities/spacing';
+import TEXT from '../../utilities/text';
 
 export default StyleSheet.create({
   background: {
@@ -15,9 +17,34 @@ export default StyleSheet.create({
     right: SPACING.medium,
   },
   tile: {
+    flex: 0.5,
     marginBottom: SPACING.medium,
   },
-  tileRight: {
-    marginLeft: SPACING.medium,
+  tileLeft: {
+    marginRight: SPACING.medium,
+  },
+  footer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    width: '100%',
+  },
+  balloon: {
+    borderWidth: BORDER.width,
+    borderRadius: 100,
+    borderColor: COLORS.black,
+    backgroundColor: COLORS.white,
+    paddingTop: SPACING.small,
+    paddingLeft: SPACING.medium,
+    paddingRight: SPACING.medium,
+    paddingBottom: SPACING.small,
+  },
+  text: {
+    fontFamily: TEXT.input.fontFamily,
+    fontSize: TEXT.input.size,
+    textAlign: 'center',
   },
 });
