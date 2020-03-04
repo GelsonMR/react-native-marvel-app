9fc7d39343e6742d50eb83399eff7e848f4327ae
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-03-03 23:02:40 -0300

    Added HeroTile to Character Screen

--- src/screens/characters/screen.js
+++ src/screens/characters/screen.js
@@ -6,8 +6,11 @@ import {
 } from 'react-native';
 
 import InputText from '../../components/InputText';
+import HeroTile from '../../components/HeroTile';
 import COLORS from '../../utilities/colors';
 import STYLES from './styles';
+
+const defaultAvatar = require('../../../assets/images/default-avatar.png');
 
 export default class CharactersScreen extends Component {
   render() {
@@ -21,7 +24,23 @@ export default class CharactersScreen extends Component {
         <FlatList
           data={[]}
           keyExtractor={(item, index) => index.toString()}
-          renderItem={({ index }) => (<Text>{index}</Text>)}
+          renderItem={({ index, item }) => {
+            const isImageAvailable = item.thumbnail.path.indexOf('image_not_available') === -1;
+            const avatar = { uri: `${item.thumbnail.path}.${item.thumbnail.extension}` }
+            const source = isImageAvailable ? avatar : defaultAvatar;
+
+            return (
+              <HeroTile
+                name={item.name}
+                source={source}
+                style={[
+                  STYLES.tile,
+                  index % 2 && STYLES.tileRight,
+                ]}
+              />
+            );
+          }}
+          numColumns={2}
           contentContainerStyle={STYLES.background}
         />
       </>
