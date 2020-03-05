55fabbe155590e4750c50db07952b20dee02b52b
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2020-03-05 14:25:40 -0300

    Added marvel's security params test

---
+++ src/api/api.test.js
@@ -0,0 +1,22 @@
+import config from './config';
+import api from './index';
+
+describe('API service tests', () => {
+  it('Generates proper API security parameters', () => {
+    const TIMESTAMP = 1583426722880;
+    const realDateNow = Date.now.bind(global.Date);
+    const dateNowStub = jest.fn(() => TIMESTAMP);
+    global.Date.now = dateNowStub;
+  
+    expect(Date.now()).toBe(TIMESTAMP);
+    expect(dateNowStub).toHaveBeenCalled();
+  
+    expect(config.MARVEL.params()).toStrictEqual({
+      ts: TIMESTAMP,
+      apikey: 'public',
+      hash: 'a6fb03b5588d346493847e5c4eb07465',
+    });
+  
+    global.Date.now = realDateNow;
+  });
+})
