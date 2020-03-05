c84e4ecf6db6b5d07a23bbed986b176b2dd05850
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-03-04 21:31:43 -0300

    Added Detail screen to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import CharactersScreen from '../screens/characters/screen';
+import DetailScreen from '../screens/detail/screen';
 import Header from '../components/Header';
 
 const Stack = createStackNavigator();
@@ -11,6 +12,7 @@ const App = () => (
   <NavigationContainer>
     <Stack.Navigator screenOptions={{ header: Header }}>
       <Stack.Screen name="Characters..." component={CharactersScreen} />
+      <Stack.Screen name="Detail" component={DetailScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
