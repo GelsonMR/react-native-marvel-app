14aea9334b00d11175b4f5683ac832a55a1efade
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-03-04 21:47:09 -0300

    Added onPress feature to CharacterTile

--- src/components/CharacterTile/index.js
+++ src/components/CharacterTile/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import { TouchableNativeFeedback } from 'react-native-gesture-handler';
 import PropTypes from 'prop-types';
 
 import STYLES from './styles';
@@ -8,20 +9,32 @@ const CharacterTile = ({
   source,
   name,
   style,
-}) => (
-  <View style={[STYLES.container, style]}>
-    {source && (
-      <Image
-        source={source}
-        style={STYLES.image}
-        resizeMode="cover"
-      />
-    )}
-    <View style={STYLES.balloon}>
-      <Text style={STYLES.text}>{name}</Text>
+  onPress,
+}) => {
+  const content = (
+    <>
+      {source && (
+        <Image
+          source={source}
+          style={STYLES.image}
+          resizeMode="cover"
+        />
+      )}
+      <View style={STYLES.balloon}>
+        <Text style={STYLES.text}>{name}</Text>
+      </View>
+    </>
+  );
+  return (
+    <View style={[STYLES.container, style]}>
+      {
+        onPress
+          ? <TouchableNativeFeedback onPress={onPress}>{content}</TouchableNativeFeedback>
+          : content
+      }
     </View>
-  </View>
-);
+  );
+};
 
 CharacterTile.propTypes = {
   style: PropTypes.oneOfType([
@@ -41,11 +54,13 @@ CharacterTile.propTypes = {
     PropTypes.node,
   ]),
   name: PropTypes.string.isRequired,
+  onPress: PropTypes.func,
 };
 
 CharacterTile.defaultProps = {
   style: null,
   source: null,
+  onPress: null,
 };
 
 export default CharacterTile;
